87433aa047838aaa9024a264758665be3a8d06b3
simonrosengren <simonrosengren123@gmail.com>
2017-09-20 17:04:17 +0200

    Adding projectile thrower

--- Assets/Scripts/PlayerMovement.cs
+++ Assets/Scripts/PlayerMovement.cs
@@ -92,11 +92,11 @@ public class PlayerMovement : MonoBehaviour
 
     }
 
-    void OnAnimatorIK()
-    {
-        animator.SetIKPositionWeight(AvatarIKGoal.RightHand, 1);
-        animator.SetIKPosition(AvatarIKGoal.RightHand, hangingPos);
-    }
+    //void OnAnimatorIK()
+    //{
+    //    animator.SetIKPositionWeight(AvatarIKGoal.RightHand, 1);
+    //    animator.SetIKPosition(AvatarIKGoal.RightHand, hangingPos);
+    //}
 
 
     /*The method, which is triggered by entering a climbTrigger, will cast Rays higher and higher and stop 
@@ -131,13 +131,13 @@ public class PlayerMovement : MonoBehaviour
                 Debug.Log(lastRayHitPoint);
                 lastRayHitPoint = hitObj.point;
                 playerRb.constraints = RigidbodyConstraints.FreezeAll;
-                transform.position = lastRayHitPoint - new Vector3(0, 0.6f, 0) - (transform.forward * 0.2f);
+                transform.position = lastRayHitPoint - new Vector3(0, 0.5f, 0) - (transform.forward * 0.2f);
                 IsHanging = true;
                 animator.SetBool("IsHanging", true);
                 hangingPos = lastRayHitPoint;
             }
             playerRb.constraints = RigidbodyConstraints.FreezeAll;
-            transform.position = lastRayHitPoint - new Vector3(0, 0.2f, 0) - (transform.forward * 10);
+            transform.position = lastRayHitPoint - new Vector3(0, 0.5f, 0) - (transform.forward * 10);
             animator.SetBool("IsHanging", true);
         }
     }
