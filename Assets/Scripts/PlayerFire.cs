08fdc3a98dca351118cbe7048857e09fd388f564
Zalasar <xalaxar@hotmail.com>
2017-09-27 16:25:00 +0200

    Basic fire mechanics completed

--- Assets/Scripts/PlayerFire.cs
+++ Assets/Scripts/PlayerFire.cs
@@ -9,12 +9,12 @@ public class PlayerFire : MonoBehaviour {
     public bool matchLit = false;
     private Transform matchLocation;
     private float timer = 1;
-    public Vector3 vec;
+    
 	// Use this for initialization
 	void Start () {
         
-        matchLocation = transform.Find("MatchSpawn");
-        vec = matchLocation.position;
+        matchLocation = transform.Find("RightHand");
+        
 	}
 	
 	// Update is called once per frame
@@ -34,17 +34,21 @@ public class PlayerFire : MonoBehaviour {
             else if(!matchLit)
             {
                 matchLit = true;
-                newMatch = Instantiate(match, matchLocation.transform.position, Quaternion.identity, matchLocation.transform.parent) as GameObject;
-                //newMatch.transform.position = matchLocation.transform.position;
-                //newMatch.transform.parent = matchLocation;
-                //matchLocation.transform.parent = transform;
-                //newMatch.transform.position = matchLocation.localPosition;
-                //newMatch.transform.position = matchLocation.transform.position;
-                //newMatch.transform.position = new Vector3(matchLocation.position.x, matchLocation.position.y, matchLocation.position.z);
+                newMatch = Instantiate(match, matchLocation.position, Quaternion.identity) as GameObject;
+                newMatch.transform.parent = matchLocation;
+                
                 timer = 1;
             }
             
-        }      
+        }
+      
 		
 	}
+    void OnTriggerEnter(Collider other)
+    {
+        if (other.tag == "flammable" || other.tag == "ice")
+        {
+            other.SendMessageUpwards("SetFire");
+        }
+    }
 }
