d60dc6883be954894e75215de6736307939f6070
godofnanners <casper.martensson@telia.com>
2017-10-12 14:01:13 +0200

    fading objects done
    the camera can now fade objects away that blocks the view of the player

--- Assets/Scripts/FadeObjectsBetween.cs
+++ Assets/Scripts/FadeObjectsBetween.cs
@@ -14,18 +14,18 @@ public class FadeObjectsBetween : MonoBehaviour {
     List<RaycastHit> transObjects;
     // Use this for initialization
     void Start () {
-		
+        transObjects = new List<RaycastHit>();
 	}
 	
 	// Update is called once per frame
 	void Update () {
-        Vector3 playerV = Player.transform.position;
-        Vector3 cameraV = Camera.transform.position;
+        playerV = Player.transform.position;
+        cameraV = Camera.transform.position;
         Vector3 direction = playerV-cameraV;
 
         RaycastHit[] hits;
 
-        //AddAlpha();
+        AddAlpha();
 
         if (Physics.Linecast(cameraV, playerV))
         {
@@ -35,27 +35,35 @@ public class FadeObjectsBetween : MonoBehaviour {
             
                 for(int i = 0; i < hits.Length; i++)
                 {
-                Renderer rend = hits[i].transform.GetComponent<Renderer>();
-
-
-
-                // Change the material of all hit colliders
-                // to use a transparent shader.
-
-                rend.material.shader = Shader.Find("Transparent/Diffuse");
-                Color tempColor = rend.material.color;
-                tempColor.a = 0.3F;
-                rend.material.color = tempColor;
-                
-                foreach (RaycastHit g in transObjects)
-                {
-                    if (!g.Equals(hits[i]))
+                    if (hits[i].transform.gameObject!=Player)
                     {
-                        transObjects.Add(hits[i]);
-                    }
-                    
-                }
+                        Renderer rend = hits[i].transform.GetComponent<Renderer>();
+
+
+
+                        // Change the material of all hit colliders
+                        // to use a transparent shader.
+
+                        rend.material.shader = Shader.Find("Transparent/Diffuse");
+                        Color tempColor = rend.material.color;
+                        tempColor.a = 0.3F;
                 
+                        rend.material.color=Color.Lerp(rend.material.color, tempColor, 10f *Time.deltaTime);
+                
+
+                
+
+                        for (int c = 0; c < transObjects.Count; c++)         
+                        {
+                            if (transObjects[c].Equals(hits[i]))
+                            {
+                                break;
+                            }
+                        }
+
+                        transObjects.Add(hits[i]);
+
+                    }
                     
                 }
 
@@ -71,15 +79,13 @@ public class FadeObjectsBetween : MonoBehaviour {
             for (int i = 0; i < transObjects.Count; i++)
             {
             
-                        Renderer rend = transObjects[i].transform.GetComponent<Renderer>();
-                        Color tempColor = rend.material.color;
-                        tempColor.a += 0.01F;
-                        rend.material.color = tempColor;
+                Renderer rend = transObjects[i].transform.GetComponent<Renderer>();
 
-                        if (rend.material.color.a > 0)
-                        {
-                            transObjects.Remove(transObjects[i]);
-                        }
+                rend.material.shader = Shader.Find("Standard");
+
+                
+                transObjects.Remove(transObjects[i]);
+               
                 
             
             }
