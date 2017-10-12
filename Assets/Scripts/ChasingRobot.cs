e5af582c95b5adfb0bb588b8afec390acaed4e55
simonrosengren <simonrosengren123@gmail.com>
2017-10-12 11:57:25 +0200

    Adding AI robot

---
+++ Assets/Scripts/ChasingRobot.cs
@@ -0,0 +1,43 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.AI;
+
+public class ChasingRobot : MonoBehaviour {
+
+    Transform player;               // Reference to the player's position.
+    NavMeshAgent nav;               // Reference to the nav mesh agent.
+    bool active = false;
+
+
+    void Awake()
+    {
+        // Set up the references.
+        player = GameObject.FindGameObjectWithTag("Player").transform;
+        nav = GetComponent<NavMeshAgent>();
+    }
+
+
+    void Update()
+    {
+        if (active)
+        {
+            nav.enabled = true;
+            nav.SetDestination(player.position);
+        }
+
+        else
+        {
+            nav.enabled = false;
+        }
+    }
+
+    public void SetToActive()
+    {
+        active = true;
+    }
+    public void Deactivate()
+    {
+        active = false;
+    }
+}
