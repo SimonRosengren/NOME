c5ec17653d92a6ca16ada88efbf1b5ef3ec7353b
simonrosengren <simonrosengren123@gmail.com>
2017-10-31 12:28:41 +0100

    Adding color-schemes and ID to custom analytics tool

--- Assets/Scripts/CustomAnalyticToolRecord.cs
+++ Assets/Scripts/CustomAnalyticToolRecord.cs
@@ -5,21 +5,24 @@ using System.IO;
 
 public class CustomAnalyticToolRecord : MonoBehaviour {
 
+    public bool recordMovement;
+    public float saveRate = 0.1f;
+
     //Player goes here if you want to track player movement
     [SerializeField]
     Transform objectToTrack;
+
+    int sessionID;
 
     //Path to text file
     string path = @"CustomAnalyticsTool\Movement.txt";
 
     float timer = 0;
 
-    public float saveRate = 0.1f;
-
 
     void Start()
     {
-
+        CreateSessionID();
     }
 
     void Update()
@@ -29,6 +32,14 @@ public class CustomAnalyticToolRecord : MonoBehaviour {
 
     void SaveToFile()
     {
+        if (recordMovement)
+        {
+            savePositions();
+        }
+
+    }
+    public void savePositions()
+    {
         timer += Time.deltaTime;
 
         if (timer >= saveRate)
@@ -36,9 +47,20 @@ public class CustomAnalyticToolRecord : MonoBehaviour {
             using (System.IO.StreamWriter file =
                 new System.IO.StreamWriter(path, true))
             {
-                file.WriteLine(objectToTrack.position + "\n");
+                file.WriteLine(objectToTrack.position + Vector3.up + "\n");
             }
             timer = 0;
+        }
+    }
+    //This method creates the ID. For increased safety this should be done with a GUID Instead, 
+    //however I find this exessive as of now
+    void CreateSessionID()
+    {
+        sessionID = (int)Random.Range(0, 9999999);
+        using (System.IO.StreamWriter file =
+    new System.IO.StreamWriter(path, true))
+        {
+            file.WriteLine("SESSION " + sessionID + "\n");
         }
     }
 }
--- Assets/Scripts/CustomAnalyticsToolPresenter.cs
+++ Assets/Scripts/CustomAnalyticsToolPresenter.cs
@@ -5,16 +5,34 @@ using UnityEngine;
 
 public class CustomAnalyticsToolPresenter : MonoBehaviour {
 
+    public class PlayerPath
+    {
+        public readonly int ID;
+        public readonly Color color;
+        public List<Vector3> path;
+
+        public PlayerPath(int id)
+        {
+            this.ID = id;
+            this.color = UnityEngine.Random.ColorHSV();
+            this.path = new List<Vector3>();
+        }
+    }
+
     string[] positions;
     string[] separators = { " ", "(", ")", "," };
 
     List<Vector3> parsedPositions;
+    List<PlayerPath> playerPaths;
 
     //Path to text file
     string posPath = @"CustomAnalyticsTool\Movement.txt";
 
+
+
     void Start () {
         parsedPositions = new List<Vector3>();
+        playerPaths = new List<PlayerPath>();
         ReadInPositions();
         ParseToPositions();
 
@@ -34,32 +52,47 @@ public class CustomAnalyticsToolPresenter : MonoBehaviour {
     //Tries to parse them into vector3 and isnerts them into list
     void ParseToPositions()
     {
+        int currentSession = 0;
+
         // i+=2 because it save a empty line between every line, so we want to skip those
         for (int i = 0; i < positions.Length; i+=2)
         {
-            if(positions[i] != "newplayer")
+            if (positions[i].Contains("SESSION"))
+            {
+                string[] sess = positions[i].Split(separators, StringSplitOptions.RemoveEmptyEntries);
+                int id;
+                int.TryParse(sess[1], out id);
+                playerPaths.Add(new PlayerPath(id));
+                currentSession++;
+            }
+            else if (!positions[i].Contains("SESSION"))
             {
                 string[] xyz = positions[i].Split(separators, StringSplitOptions.RemoveEmptyEntries);
                 Vector3 p = new Vector3();
                 float.TryParse(xyz[0], out p.x);
                 float.TryParse(xyz[1], out p.y);
                 float.TryParse(xyz[2], out p.z);
-                parsedPositions.Add(p);
-            }
-            else if(positions[i] == "newplayer")
-            {
+                //parsedPositions.Add(p);
+                playerPaths[currentSession - 1].path.Add(p);
                 
             }
-
         }
 
     }
 
     void DrawLinesBetweenPoints()
     {
-        for (int i = 0; i < parsedPositions.Count - 1; i++)
+        //for (int i = 0; i < parsedPositions.Count - 1; i++)
+        //{
+        //    Debug.DrawLine(parsedPositions[i], parsedPositions[i + 1], Color.red);
+        //}
+        for (int i = 0; i < playerPaths.Count; i++)
         {
-            Debug.DrawLine(parsedPositions[i], parsedPositions[i + 1], Color.red);
+            Color pColor = UnityEngine.Random.ColorHSV();
+            for (int j = 0; j < playerPaths[i].path.Count - 1; j++)
+            {
+                Debug.DrawLine(playerPaths[i].path[j], playerPaths[i].path[j + 1], playerPaths[i].color);
+            }
         }
     }
 }
