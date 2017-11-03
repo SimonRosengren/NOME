de32698d53d0d685e260357df7f5f162de33ed39
simonrosengren <simonrosengren123@gmail.com>
2017-11-03 13:57:08 +0100

    Adding fade effect to scene change

---
+++ Assets/Scripts/LoadIndoorScene.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class LoadIndoorScene : MonoBehaviour {
+
+    public Animator anim;
+    public float fadeTimer = 1;
+    public string scene = "Emils scene";
+
+    void OnTriggerEnter(Collider other)
+    {
+        if (other.tag == "Player")
+        {
+            StartCoroutine(FadeToScene());
+        }
+    }
+
+    IEnumerator FadeToScene()
+    {
+        anim.SetBool("FadeOut", true);
+        yield return new WaitForSecondsRealtime(fadeTimer);
+        SceneManager.LoadScene(scene);
+    }
+}
