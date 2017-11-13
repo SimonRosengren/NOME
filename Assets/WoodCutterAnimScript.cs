985d13914e782f4dc5dd360afc5ba4b48c95bd37
epain17 <epain17@gmail.com>
2017-11-13 21:15:34 +0100

    Woodcutter AnimScript

---
+++ Assets/WoodCutterAnimScript.cs
@@ -0,0 +1,51 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class WoodCutterAnimScript : MonoBehaviour
+{
+
+    public Transform endTransForm;
+    public Animator anim;
+    public bool start;
+    
+    public float speed;
+
+    // Use this for initialization
+    void Start()
+    {
+        anim = GetComponent<Animator>();
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        Vector3 direction = endTransForm.position - this.transform.position;
+        direction.y = 0;
+        float step = speed * Time.deltaTime;
+
+
+
+        if (anim.GetCurrentAnimatorStateInfo(0).IsName("Start_Walking_") && direction.magnitude > 1 ||
+            anim.GetCurrentAnimatorStateInfo(0).IsName("Walking_Loop_") && direction.magnitude > 1)
+        {
+            transform.position = Vector3.MoveTowards(transform.position, endTransForm.position, step);
+
+        }
+
+        else if(direction.magnitude < 1)
+        { 
+            anim.SetBool("isActive", false);
+            anim.SetBool("StartWalk", false);
+            anim.SetBool("StopWalking", true);
+            anim.SetBool("StartCutting", true);
+
+        }
+
+        
+        
+
+
+    }
+}
