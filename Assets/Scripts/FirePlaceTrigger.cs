0d7f5491c97915a876874070ce044a7fa1e2c33c
simonrosengren <simonrosengren123@gmail.com>
2017-11-28 15:16:45 +0100

    adding rain effect

---
+++ Assets/Scripts/FirePlaceTrigger.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class FirePlaceTrigger : MonoBehaviour {
+
+    ParticleSystem sprinkler;
+    Transform sprinklerTransform;
+
+    [SerializeField]RobotBehaviour robot;
+
+
+	void Start () {
+
+	}
+
+	void Update () {
+		
+	}
+
+    public void ActivateSprinkler()
+    {
+        Instantiate(sprinkler, sprinklerTransform);
+        Invoke("DeactivateRobot", 2);
+    }
+
+    public void DeactivateRobot()
+    {
+        robot.wet = true;
+    }
+}
