bd040f87017215f011efdc49e9dd0eaf85f73e56
felixhelgesson <felix.helgesson@gmail.com>
2017-12-10 19:06:46 +0100

    snabbfixar

--- Assets/Scripts/FocusTarget.cs
+++ Assets/Scripts/FocusTarget.cs
@@ -2,13 +2,15 @@
 using System.Collections.Generic;
 using UnityEngine;
 
-public class FocusTarget : MonoBehaviour {
-    
+public class FocusTarget : MonoBehaviour
+{
+
     public GameObject target;
     public float panSpeed;
     public float panTime;
+    public AudioSource aS;
 
-    CameraScriptFree camScript;          
+    CameraScriptFree camScript;
     bool cutsceneDone = false;
     bool inCutscene = false;
     float timer;
@@ -18,7 +20,7 @@ public class FocusTarget : MonoBehaviour {
         GameObject cam = GameObject.FindGameObjectWithTag("MainCamera");
         camScript = cam.GetComponent<CameraScriptFree>();
     }
-    void Update ()
+    void Update()
     {
         if (inCutscene)
         {
@@ -30,18 +32,35 @@ public class FocusTarget : MonoBehaviour {
                 camScript.cameraSpeed = 10f;
                 inCutscene = false;
                 cutsceneDone = true;
+                
             }
         }
-	}
+    }
 
+
+    public static IEnumerator FadeOutMusic(AudioSource audioSource, float FadeTime)
+    {
+        float startVolume = audioSource.volume;
+
+        while (audioSource.volume > 0)
+        {
+            audioSource.volume -= startVolume * Time.deltaTime / FadeTime;
+
+            yield return null;
+        }
+
+        audioSource.Stop();
+        audioSource.volume = startVolume;
+    }
     private void OnTriggerExit(Collider other)
     {
         if (other.tag == "Player" && !cutsceneDone)
         {
+            FadeOutMusic(aS, 0.5f);
             camScript.cameraSpeed = panSpeed;
             camScript.Target = target;
             timer = panTime;
             inCutscene = true;
-        }        
+        }
     }
 }
